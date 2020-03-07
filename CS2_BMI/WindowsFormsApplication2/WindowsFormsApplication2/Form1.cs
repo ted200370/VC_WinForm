73a5d3db682b151537aa93970782f0122c3a7d37
ted200370 <61673023+ted200370@users.noreply.github.com>
2020-03-07 13:48:39 +0800

    Add files via upload

---
+++ CS2_BMI/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double w = double.Parse(textBox1.Text);
+            double h = double.Parse(textBox2.Text);
+            h = h /100;
+            double b = w / (h * h);
+            label3.Text = b.ToString();
+        }
+    }
+}
